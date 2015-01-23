f3a39e877d7335835cca54d52b5780f48e470a8b
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-01-23 20:13:38 +0100

    Added the generic singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+	protected static T instance;
+
+	public static T Instance
+	{
+		get {
+			if (instance == null) 
+			{
+				T[] res = FindObjectsOfType<T>();
+
+				if (res.Length == 0)
+					Debug.LogError(string.Format(
+						"There must be an instance of {0} in the scene", typeof(T).FullName));
+				else
+					instance = res[0];
+
+
+			}
+			return instance;
+		}
+	}
+}
