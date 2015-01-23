bc5113a034bdd9568a1b92a8f8c6b6400b1e9fd8
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-01-23 22:24:19 +0100

    Level controller

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : Singleton<Level> {
+
+	public float timeSinceLevelStart {
+		get {
+			return Time.timeSinceLevelLoad;
+		}
+	}
+}
