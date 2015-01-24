922ca829258719ec9dd71c82de0cb63bb2b5329b
CrazyNoa <ludvig.eger@gmail.com>
2015-01-24 15:01:41 +0100

    Worked more on camera script

--- Assets/RoomProperties.cs
+++ Assets/RoomProperties.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class RoomProperties : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public Rect Bounds;
+	public bool StartRoom;
+
+	void Awake () {
+		if(StartRoom)
+			Level.currentRoom = this;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnDrawGizmosSelected () {
+		Gizmos.color = new Color(0,1,0,0.3f);
+		Gizmos.DrawCube(new Vector3(Bounds.center.x,Bounds.center.y,0), new Vector3(Bounds.width, Bounds.height));
 	}
 }
--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 	 
-	public static Vector3 offset = new Vector3(0,0,-10);
-	public static Rect movementBounds;
-
+	public Vector3 offset = new Vector3(0,0,-10);
+	public Rect movementBounds;
+	
 	Swarmer Current;
-	bool coRoutineRunning;
+	public bool coRoutineRunning;
 
 	// Use this for initialization
-	void Start () {
-	
+	void OnDrawGizmosSelected () {
+		Gizmos.color = new Color(0,1,0,0.3f);
+		Gizmos.DrawCube(new Vector3(transform.position.x, transform.position.y,0), new Vector3(movementBounds.width, movementBounds.height));
 	}
 	
 	// Update is called once per frame
@@ -21,6 +22,24 @@ public class CameraControl : MonoBehaviour {
 			OnLeaderChange();
 		}
 
+		if(!coRoutineRunning)
+		{
+			if(Current.transform.position.x > transform.position.x + movementBounds.width/2 && Current.rigidbody2D.velocity.x > 0)
+				rigidbody2D.velocity = Current.rigidbody2D.velocity;
+
+			if(Current.transform.position.x < transform.position.x - movementBounds.width/2 && Current.rigidbody2D.velocity.x < 0)
+				rigidbody2D.velocity = Current.rigidbody2D.velocity;
+
+			if(Current.transform.position.y > transform.position.y + movementBounds.height/2 && Current.rigidbody2D.velocity.y > 0)
+				rigidbody2D.velocity = Current.rigidbody2D.velocity;
+
+			if(Current.transform.position.y < transform.position.y - movementBounds.height/2 && Current.rigidbody2D.velocity.y < 0)
+				rigidbody2D.velocity = Current.rigidbody2D.velocity;
+		}
+//		if(Current.transform.position.x >= transform.position.x + movementBounds.width/2)
+//		{
+//			transform.position = Current.transform.position - ;
+//		}
 
 //		if(transform.position > Level.currentRoom)
 //
@@ -31,21 +50,46 @@ public class CameraControl : MonoBehaviour {
 	void OnLeaderChange () {
 		Current = SwarmLeaderController.Leader;
 		Debug.Log("Leader Changed");
-		MoveCameraTo(Current.transform.position+offset, 10);
+		MoveCameraTo(Current.transform, 10);
 	}
 
-	void MoveCameraTo (Vector3 destination, float speed) {
+	void MoveCameraTo (Transform destination, float speed) {
 		StopAllCoroutines();
 		StartCoroutine(Move(destination, speed));
 	}
 
-	IEnumerator Move (Vector3 destination, float speed) {
+	IEnumerator Move (Transform destination, float speed) {
+
+		//TODO Fixa så kameran inte hackar vid bankanter;
+
 		coRoutineRunning = true;
-		while(transform.position != destination)
+		rigidbody2D.velocity = Vector2.zero;
+		while(Vector3.Distance(transform.position-offset, destination.position) > 1)
 		{
-			transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime*speed);
+			Vector3 delta = Vector3.Lerp(transform.position, destination.position+offset, Time.deltaTime*speed) - transform.position;
+
+//			if(CheckCameraBounds(transform.position + delta))
+//			{
+//				transform.position -= delta;
+//				return false;
+//			}
+//			else
+				transform.position += delta;
+
 			yield return null;
+
+
 		}
 		coRoutineRunning = false;
+	}
+
+	bool CheckCameraBounds(Vector3 destination) {
+		//if(!Level.currentRoom.Bounds.Contains(Vector2.Lerp(transform.position, destination, Time.deltaTime*speed))
+		if(!Level.currentRoom.Bounds.Contains(camera.ScreenToWorldPoint(Vector3.zero)))
+			return true;
+		else if(!Level.currentRoom.Bounds.Contains(camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth,camera.pixelHeight,1))))
+			return true;
+		else
+			return false;
 	}
 }
