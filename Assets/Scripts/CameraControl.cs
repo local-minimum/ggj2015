304d1b7ac971309aa87844d491a437afd2f32efe
CrazyNoa <ludvig.eger@gmail.com>
2015-01-24 16:30:20 +0100

    Working more on camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -6,7 +6,7 @@ public class CameraControl : MonoBehaviour {
 	public Vector3 offset = new Vector3(0,0,-10);
 	public Rect movementBounds;
 	
-	Swarmer Current;
+	Swarmer current;
 	public bool coRoutineRunning;
 
 	// Use this for initialization
@@ -18,39 +18,31 @@ public class CameraControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Current != SwarmLeaderController.Leader) {
+		if(current != SwarmLeaderController.Leader) {
 			OnLeaderChange();
 		}
 
-		if(!coRoutineRunning && Current != null)
+		if(!coRoutineRunning && current != null)
 		{
-			if(Current.transform.position.x > transform.position.x + movementBounds.width/2 && Current.rigidbody2D.velocity.x > 0)
-				rigidbody2D.velocity = Current.rigidbody2D.velocity;
-
-			if(Current.transform.position.x < transform.position.x - movementBounds.width/2 && Current.rigidbody2D.velocity.x < 0)
-				rigidbody2D.velocity = Current.rigidbody2D.velocity;
-
-			if(Current.transform.position.y > transform.position.y + movementBounds.height/2 && Current.rigidbody2D.velocity.y > 0)
-				rigidbody2D.velocity = Current.rigidbody2D.velocity;
-
-			if(Current.transform.position.y < transform.position.y - movementBounds.height/2 && Current.rigidbody2D.velocity.y < 0)
-				rigidbody2D.velocity = Current.rigidbody2D.velocity;
-		}
-//		if(Current.transform.position.x >= transform.position.x + movementBounds.width/2)
-//		{
-//			transform.position = Current.transform.position - ;
-//		}
-
-//		if(transform.position > Level.currentRoom)
+			transform.position = current.transform.position + offset;
+//			if(Current.transform.position.x > transform.position.x + movementBounds.width/2 && Current.rigidbody2D.velocity.x > 0)
+//				rigidbody2D.velocity = Current.rigidbody2D.velocity;
 //
-//		if(!coRoutineRunning)
-//			transform.position = SwarmLeaderController.Leader.transform.position+offset;
+//			if(Current.transform.position.x < transform.position.x - movementBounds.width/2 && Current.rigidbody2D.velocity.x < 0)
+//				rigidbody2D.velocity = Current.rigidbody2D.velocity;
+//
+//			if(Current.transform.position.y > transform.position.y + movementBounds.height/2 && Current.rigidbody2D.velocity.y > 0)
+//				rigidbody2D.velocity = Current.rigidbody2D.velocity;
+//
+//			if(Current.transform.position.y < transform.position.y - movementBounds.height/2 && Current.rigidbody2D.velocity.y < 0)
+//				rigidbody2D.velocity = Current.rigidbody2D.velocity;
+		}
 	}
 
 	void OnLeaderChange () {
-		Current = SwarmLeaderController.Leader;
+		current = SwarmLeaderController.Leader;
 		Debug.Log("Leader Changed");
-		MoveCameraTo(Current.transform, 10);
+		MoveCameraTo(current.transform, 10);
 	}
 
 	void MoveCameraTo (Transform destination, float speed) {
@@ -60,36 +52,18 @@ public class CameraControl : MonoBehaviour {
 
 	IEnumerator Move (Transform destination, float speed) {
 
-		//TODO Fixa så kameran inte hackar vid bankanter;
-
 		coRoutineRunning = true;
 		rigidbody2D.velocity = Vector2.zero;
 		while(Vector3.Distance(transform.position-offset, destination.position) > 1)
 		{
 			Vector3 delta = Vector3.Lerp(transform.position, destination.position+offset, Time.deltaTime*speed) - transform.position;
 
-//			if(CheckCameraBounds(transform.position + delta))
-//			{
-//				transform.position -= delta;
-//				return false;
-//			}
-//			else
-				transform.position += delta;
+			transform.position += delta;
 
 			yield return null;
 
 
 		}
 		coRoutineRunning = false;
-	}
-
-	bool CheckCameraBounds(Vector3 destination) {
-		//if(!Level.currentRoom.Bounds.Contains(Vector2.Lerp(transform.position, destination, Time.deltaTime*speed))
-		if(!Level.currentRoom.Bounds.Contains(camera.ScreenToWorldPoint(Vector3.zero)))
-			return true;
-		else if(!Level.currentRoom.Bounds.Contains(camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth,camera.pixelHeight,1))))
-			return true;
-		else
-			return false;
 	}
 }
