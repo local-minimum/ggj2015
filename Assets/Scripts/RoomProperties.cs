286953760771bd5d9a782804f9a65a8c80864430
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-01-24 16:56:48 +0100

    Rooms can add and remove swarmers

--- Assets/Scripts/RoomProperties.cs
+++ Assets/Scripts/RoomProperties.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class RoomProperties : MonoBehaviour {
 
 	public Rect Bounds;
 	public bool StartRoom;
 	public Transform foodPoint;
+	public Transform swarmHolder;
+	private HashSet<Swarmer> swarm = new HashSet<Swarmer>();
 
 	void Awake () {
 		if(StartRoom)
 			Level.currentRoom = this;
+
+		foreach (Swarmer swarmer in swarmHolder.GetComponentsInChildren<Swarmer>())
+			swarm.Add(swarmer);
 	}
 
 	void OnDrawGizmosSelected () {
 		Gizmos.color = new Color(0,1,0,0.3f);
 		Gizmos.DrawCube(new Vector3(Bounds.center.x,Bounds.center.y,0), new Vector3(Bounds.width, Bounds.height));
+	}
+
+	public void RemoveSwarmer(Swarmer swarmer) {
+		swarm.Remove(swarmer);
+	}
+
+	public void AddSwarmer(Swarmer swarmer) {
+		swarm.Add(swarmer);
+		swarmer.transform.parent = swarmHolder;
 	}
 }
---
+++ Assets/Scripts/food/FoodRoomMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodRoomMovement : MonoBehaviour {
+
+	RoomProperties room;
+	public float turnHomeDistance = 10f;
+	public float forceFactor = 40f;
+	public float forceFrequency = 1f;
+	private float seed;
+
+	public float torqueFrequency = 2f;
+	public float torqueFactor = 100f;
+
+	// Use this for initialization
+	void Start () {
+		room = GetComponentInParent<RoomProperties>();
+		seed = Random.value * 1000f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AddForce();
+		AddTorque();
+	}
+
+	void AddForce() {
+		Vector2 force = new Vector2(Mathf.PerlinNoise(seed, forceFrequency * Level.timeSinceLevelStart) - Level.PerlinValue, Mathf.PerlinNoise(forceFrequency * Level.timeSinceLevelStart, seed) - Level.PerlinValue);
+		Vector2 relativePosition = transform.position - room.foodPoint.position;
+		
+		if (relativePosition.magnitude < turnHomeDistance)
+			force = (force + relativePosition * -1).normalized;
+		
+		rigidbody2D.AddForce(force * Time.deltaTime * forceFactor);
+	}
+
+	void AddTorque() {
+		rigidbody2D.AddTorque((Mathf.PerlinNoise(seed * 10f, Level.timeSinceLevelStart * torqueFrequency) - Level.PerlinValue) * torqueFactor);
+	}
+}
